cf6fee9280f1b41956dcde62ba24c6aa57ed445d
Sneha Sarkar <snehasarkar081995@gmail.com>
2023-06-08 16:01:12 +0530

    requirments & iinstallation done

--- template.py
+++ template.py
@@ -8,9 +8,9 @@ project_name = "textSummarizer"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/__init__.py",                      #constructor file
+    f"src/{project_name}/components/__init__.py",           #constructor file
+    f"src/{project_name}/utils/__init__.py",                #constructor file
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging//__init__.py",
     f"src/{project_name}/config/__init__.py",
@@ -18,7 +18,7 @@ list_of_files = [
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
-    "config/config.yaml",
+    "config/config.yaml",   #keeps model related parameters
     "params.yaml",
     "app.py",
     "main.py",
